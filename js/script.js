c3c46137094343a6384d14445e51c630c96cae50
Dominic Myers <annoyingmouse@gmail.com>
2026-01-08 16:59:46 +0000

    📝 (style.css): Add new main background color variable --main-bg-colour 📝 (style.css): Update font-family in body to include Gibson font 📝 (style.css): Adjust padding and font size in header elements for better spacing and readability 📝 (style.css): Update padding and font size in language switcher button for better styling 📝 (style.css): Update padding, border-radius, and font size in nav button for better styling 📝 (style.css): Add new styles for logo and navigation elements in header 📝 (style.css): Add new styles for search input and button in header 📝 (index.html): Change lang attribute in html tag to "cy" for Welsh language 📝 (index.html): Add new svg and text for accessibility options in Welsh language 📝 (index.html): Add new svg and text for increasing text size in Welsh language 📝 (index.html): Update logo and navigation elements with new styles and content 📝 (index.html): Add search input and button in header for search functionality 📝 (js/script.js): Update search input placeholder text based on the selected language in the html tag

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 (() => {
   const switchers = document.querySelectorAll("#language_switcher > button");
+  const searchInput = document.getElementById("search-input")
+  if (document.documentElement.lang === "en") {
+    searchInput.placeholder = "Search"
+  }
+  if (document.documentElement.lang === "cy") {
+    searchInput.placeholder = "Chwiliwch"
+  }
   switchers.forEach((switcher) => {
     switcher.onclick = () => {
       switchers.forEach((el) => el.classList.remove("active"));
@@ -7,6 +14,12 @@
       if(switcher.hasAttribute("lang")){
         document.documentElement.lang = switcher.getAttribute("lang");
         document.body.dispatchEvent(new CustomEvent('lang-change'));
+        if(lang === "en"){
+          searchInput.placeholder = "Search"
+        }
+        if(lang === "cy"){
+          searchInput.placeholder = "Chwiliwch"
+        }
       }
     }
   })
