ce304bfe03788ff414604ccb5b5c8ec3fb3be042
Dominic Myers <annoyingmouse@gmail.com>
2026-01-09 14:53:26 +0000

    📝 Update CSS variables in style.css for button background color 📝 Update HTML structure in index.html for button and language switcher 📝 Update JavaScript in script.js to handle text size changing functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 (() => {
+  const html = document.querySelector("html")
   const switchers = document.querySelectorAll("#language_switcher > button");
   const searchInput = document.getElementById("search-input")
   const formOptions = document.querySelectorAll("form option");
-  formOptions.forEach((option) => {
-    console.log(option.dataset)
+  const sizeChanger = document.getElementById("size_changer");
+  sizeChanger.addEventListener("click", (e) => {
+    if(sizeChanger.dataset.size === "default") {
+      sizeChanger.dataset.size = "enlarged"
+      html.style.fontSize = "150%"
+    } else {
+      sizeChanger.dataset.size = "default"
+      html.style.fontSize = "100%"
+    }
+    sizeChanger.querySelectorAll("span").forEach(el => {
+      if(sizeChanger.dataset.size === el.dataset.size) {
+        el.style.display = "unset"
+      } else {
+        el.style.display = "none"
+      }
+    })
   })
   if (document.documentElement.lang === "en") {
     searchInput.placeholder = "Search"
@@ -48,4 +63,9 @@
       }
     }
   })
+
+  // console.log(html)
+  // html.style.fontSize = "150%"
+  // console.log(html.style.fontSize)
+
 })()
