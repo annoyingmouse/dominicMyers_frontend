ba0876f4bd970b3353bb84b79fdcdfc6a82842f2
Dominic Myers <annoyingmouse@gmail.com>
2026-01-09 07:55:04 +0000

    📝 (style.css): Add new custom CSS variables for form background and text color 📝 (style.css): Update CSS styles for responsiveness and layout improvements 📝 (index.html): Change lang attribute to "en" for English language 📝 (index.html): Add new content and form structure for SCWonline website 📝 (script.js): Update script to dynamically change form options based on selected language

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 (() => {
   const switchers = document.querySelectorAll("#language_switcher > button");
   const searchInput = document.getElementById("search-input")
+  const formOptions = document.querySelectorAll("form option");
+  console.log(formOptions);
+  formOptions.forEach((option) => {
+    console.log(option.dataset)
+  })
   if (document.documentElement.lang === "en") {
     searchInput.placeholder = "Search"
+    formOptions.forEach((option) => {
+      if(option.dataset.lang === "langEn") {
+        option.textContent = option.dataset.langEn;
+      }
+    })
   }
   if (document.documentElement.lang === "cy") {
     searchInput.placeholder = "Chwiliwch"
+    formOptions.forEach((option) => {
+      if(option.dataset.lang === "langCy") {
+        option.textContent = option.dataset.langCy;
+      }
+    })
   }
   switchers.forEach((switcher) => {
     switcher.onclick = () => {
@@ -14,11 +29,21 @@
       if(switcher.hasAttribute("lang")){
         document.documentElement.lang = switcher.getAttribute("lang");
         document.body.dispatchEvent(new CustomEvent('lang-change'));
-        if(lang === "en"){
+        if(document.documentElement.lang === "en"){
           searchInput.placeholder = "Search"
+          // formOptions.forEach((option) => {
+          //   if(option.dataset.lang === "langEn") {
+          //     option.textContent = option.dataset.langEn;
+          //   }
+          // })
         }
-        if(lang === "cy"){
+        if(document.documentElement.lang === "cy"){
           searchInput.placeholder = "Chwiliwch"
+          // formOptions.forEach((option) => {
+          //   if(option.dataset.lang === "langCy") {
+          //     option.textContent = option.dataset.langCy;
+          //   }
+          // })
         }
       }
     }
