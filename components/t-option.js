b1912012ec7a2c061967b51ab754e7b6f5c8584c
Dominic Myers <annoyingmouse@gmail.com>
2026-01-19 04:10:51 +0000

    ✨ (components): introduce custom elements TOption and TSpan to handle language translations for options and spans respectively.

---
+++ components/t-option.js
@@ -0,0 +1,39 @@
+export class TOption extends HTMLOptionElement {
+  constructor() {
+    super();
+    this._onMutation = () => this.render();
+  }
+
+  connectedCallback() {
+    Promise.resolve().then(() => {
+      if (!this.dataset.default) {
+        this.dataset.default = this.innerText.trim();
+      }
+      this.render();
+    });
+    this._observer = new MutationObserver(this._onMutation);
+    this._observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["lang"],
+    });
+  }
+
+  render() {
+    const currentLang = (document.documentElement.lang || "en")
+      .toLowerCase()
+      .split("-")[0];
+    const translation = this.dataset[currentLang];
+    const defaultValue = this.dataset.default;
+    const targetText = translation || defaultValue;
+    if (targetText && this.innerText !== targetText) {
+      this.innerText = targetText;
+    }
+  }
+  disconnectedCallback() {
+    if (this._observer) {
+      this._observer.disconnect();
+    }
+  }
+}
+
+window.customElements.define("t-option", TOption, { extends: "option" });
