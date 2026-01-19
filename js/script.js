8f157443af3fb13e5d8c43f08d40966924264e4f
Dominic Myers <annoyingmouse@gmail.com>
2026-01-19 08:10:36 +0000

    📝 (style.css): Update font paths to be relative to the CSS file location 📝 (index.html): Comment out unused font preload links for optimization 📝 (index.html): Add missing quotation mark in script tag and fix script type attribute 📝 (index.html): Add missing 'module' type in script tag for module script 📝 (index.html): Add 'novalidate' attribute to form element for form validation 📝 (index.html): Add 'required' attribute to select options for form validation 📝 (script.js): Refactor form validation logic and error messages for better user experience 📝 (script.js): Add form validation using Pristine library and custom error messages 📝 (script.js): Update language switcher functionality to change form validation messages based on selected language

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 (() => {
+  // VARIABLES
   const html = document.documentElement;
-  const switchers = document.querySelectorAll("#language_switcher > button");
   const sizeChanger = document.getElementById("size_changer");
+  const switchers = document.querySelectorAll("#language_switcher > button");
   const titleElement = document.getElementsByTagName("title")[0];
-
-  const updateLanguage = (lang) => {
-    const langKey = lang.charAt(0).toUpperCase() + lang.slice(1); // 'en' -> 'En', 'cy' -> 'Cy'
-
-    // Update Document Title
-    const titleText = titleElement.dataset[`lang${langKey}`];
-    if (titleText) document.title = titleText;
-  };
-
+  const formElement = document.getElementById("sign_up");
+  // ACCESSIBILITY
   sizeChanger.addEventListener("click", () => {
     const isDefault = sizeChanger.dataset.size === "default";
     const newSize = isDefault ? "enlarged" : "default";
@@ -23,20 +17,81 @@
       el.style.display = el.dataset.size === newSize ? "unset" : "none";
     });
   });
-
+  // FORM
+  // const defaultConfig = {
+  //   // class of the parent element where the error/success class is added
+  //   classTo: "form-group",
+  //   errorClass: "has-danger",
+  //   successClass: "has-success",
+  //   // class of the parent element where error text element is appended
+  //   errorTextParent: "form-group",
+  //   // type of element to create for the error text
+  //   errorTextTag: "div",
+  //   // class of the error text element
+  //   errorTextClass: "text-help",
+  // };
+  Pristine.addMessages("cy", {
+    required: "Mae angen y maes hwn",
+    email: "Mae angen cyfeiriad e-bost dilys ar y maes hwn",
+    number: "Mae angen rhif ar y maes hwn",
+    integer: "Mae angen gwerth cyfanrif ar y maes hwn",
+    url: "Mae angen URL gwefan ddilys ar y maes hwn",
+    tel: "Mae angen rhif ffôn dilys yn y maes hwn",
+    maxlength: "This fields length must be < ${1}",
+    minlength: "Rhaid i hyd y maes hwn fod yn > ${1}",
+    min: "Isafswm gwerth ar gyfer y maes hwn yw ${1}",
+    max: "Gwerth mwyaf y maes hwn yw ${1}",
+    pattern: "Cydweddwch y fformat gofynnol",
+    equals: "Nid yw'r ddau faes yn cyfateb",
+    default: "Rhowch y gwerth cywir"
+  })
+  Pristine.setLocale("en")
+  const pristine = new Pristine(formElement, {
+    classTo: "form-group",
+    errorClass: "has-danger",
+    successClass: "has-success",
+    errorTextParent: "form-group",
+    errorTextTag: "div",
+    errorTextClass: "error-message",
+  });
+  pristine.addValidator(
+    document.getElementById("dob"),
+    (value) => {
+      if (value) {
+        const today = Temporal.Now.plainDateISO();
+        const dob = Temporal.PlainDate.from(value)
+        const age = today.since(dob, { largestUnit: "years" }).years;
+        return age >=18 && age <= 67;
+      }
+    },
+    () => html.lang === "en" ? "Age must be between 18 and 67": "Rhaid i'r oedran fod rhwng 18 a 67"
+  );
+  formElement.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let valid = pristine.validate();
+    if (!valid) {
+      console.warn(html.lang === "en" ? "There are invalid entries in the form.": "Mae cofnodion annilys yn y ffurflen." )
+    }
+  })
+  // LANGUAGE
+  const updateLanguage = (lang) => {
+    const langKey = lang.charAt(0).toUpperCase() + lang.slice(1); // 'en' -> 'En', 'cy' -> 'Cy'
+    // Update Document Title
+    const titleText = titleElement.dataset[`lang${langKey}`];
+    if (titleText) document.title = titleText;
+  };
   switchers.forEach((switcher) => {
     switcher.onclick = () => {
       const lang = switcher.getAttribute("lang");
       if (!lang) return;
-
       switchers.forEach((el) => el.classList.remove("active"));
       switcher.classList.add("active");
-
       html.lang = lang;
+      Pristine.setLocale(lang)
       updateLanguage(lang);
       document.body.dispatchEvent(new CustomEvent("lang-change"));
     };
   });
-
   updateLanguage(html.lang || "en");
+
 })();
